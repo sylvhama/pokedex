00157fc15a6768508eecdf4d3ed6b6908b7a49c3
EC2 Default User <ec2-user@ip-172-31-43-110.us-east-2.compute.internal>
2019-01-24 19:15:04 +0000

    Prisma: deployed and generated

---
+++ backend/prisma/generated/prisma-client/index.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "Pokemon",
+    embedded: false
+  },
+  {
+    name: "Type",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `https://us1.prisma.sh/sylvain-hamann/pokedex/dev`
+});
+exports.prisma = new exports.Prisma();
